815ba12bcfd63629a46cfafa2821f037a8f2f33b
selpiameilani <114548273+selpiameilani@users.noreply.github.com>
2023-10-03 14:06:21 +0700

    Add files via upload

---
+++ tugas2_selpiameilani.js
@@ -0,0 +1,9 @@
+var firstName = "Selpia";
+var lastName = "Meilani";
+var age = 19;
+var isMarried = false;
+
+console.log("Nama Depan: " + firstName);
+console.log("Nama Belakang: " + lastName);
+console.log("Umur: " + age);
+console.log("Status Pernikahan: " + isMarried);
